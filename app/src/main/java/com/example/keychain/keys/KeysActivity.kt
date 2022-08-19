3b69529a67d523ead836fead1ba49a7272f40e98
bmackint <Billy.mackintosh@hotmail.com>
2022-08-18 22:53:25 -0400

    added list view

---
+++ app/src/main/java/com/example/keychain/keys/KeysActivity.kt
@@ -0,0 +1,28 @@
+package com.example.keychain.keys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import com.example.keychain.R
+
+class KeysActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_keys)
+
+        val arrayAdapter: ArrayAdapter<*>
+        val users = arrayOf(
+            "dfgsdfaaew", "dfgsdfaaew", "dfgsdfaaew", "dfgsdfaaew",
+            "dfgsdfaaew", "dfgsdfaaew","asdfsdf"
+        )
+        //
+        var mListView = findViewById<ListView>(R.id.keysList)
+        arrayAdapter = ArrayAdapter(this,
+        android.R.layout.simple_expandable_list_item_1
+            , users)
+        mListView.adapter = arrayAdapter
+    }
+}
